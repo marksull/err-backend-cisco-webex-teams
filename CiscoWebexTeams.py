a58e6ca4947668e35159b8cc38a0e286f482b74c
Mark Sullivan <mark@sullivans.id.au>
2024-11-23 14:20:49 +1000

    Bump version

--- CiscoWebexTeams.py
+++ CiscoWebexTeams.py
@@ -24,7 +24,7 @@ from errbot.core import ErrBot
 from markdown import markdown
 from webexteamssdk.models.cards import AdaptiveCard
 
-__version__ = "1.15.0"
+__version__ = "1.16.0"
 
 log = logging.getLogger("errbot.backends.CiscoWebexTeams")
 
